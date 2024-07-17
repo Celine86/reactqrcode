ed59bfd325d399901ddc69d2caba8a1aade3c47f
Celine86 <celine.obringer@live.fr>
2024-07-17 21:33:10 +0200

    Add download feature

--- src/components/QRCodeGenerator.jsx
+++ src/components/QRCodeGenerator.jsx
@@ -1,10 +1,15 @@
 import './QRCodeGenerator.css';
 import { useState } from 'react';
 import QRCode from 'react-qr-code';
+import saveSvgAsPng from 'save-svg-as-png';
+
 
 export function QrCodeGenerator () {
 
     const [value, setValue] = useState();
+    const handleClick = () => {
+        saveSvgAsPng.saveSvgAsPng(document.getElementById("thisQRCode"), `myQRCode.png`)
+    }
     
     return (
     <main>
@@ -16,13 +21,17 @@ export function QrCodeGenerator () {
         />
         <div className="qrcode">
             {value ? (
+                <div>
                 <div className="thecode">
                     <QRCode 
+                        id="thisQRCode"
                         title="Liline's Lab"
                         value={value}
                         size={165}
                         level='L'
                     />
+                </div>
+                    <button onClick={() => handleClick()}>Télécharger</button>
                 </div>
             ) : (
                 <p>Le QRCode apparaîtra ici !</p>
