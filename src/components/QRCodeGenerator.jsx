5c6838bb5db784206fa8547c6b7364b20b759de9
Celine86 <celine.obringer@live.fr>
2024-07-18 21:16:37 +0200

    Now QRCode can be printed !

--- src/components/QRCodeGenerator.jsx
+++ src/components/QRCodeGenerator.jsx
@@ -1,11 +1,27 @@
 import './QRCodeGenerator.css';
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import QRCode from 'react-qr-code';
+import * as htmlToImage from "html-to-image";
 
 export function QrCodeGenerator () {
 
     const [value, setValue] = useState();
+    const qrCodeRef = useRef(null);
     
+    const downloadQRCode = () => {
+      htmlToImage
+        .toPng(qrCodeRef.current)
+        .then(function (dataUrl) {
+          const link = document.createElement("a");
+          link.href = dataUrl;
+          link.download = "myQRCode.png";
+          link.click();
+        })
+        .catch(function (error) {
+          console.error("Pb de création du QRCode:", error);
+        });
+    };
+
     return (
     <main>
         <h1>Entrez votre url et obtenez un QRCode !</h1>
@@ -14,16 +30,19 @@ export function QrCodeGenerator () {
             onChange={(e) => setValue(e.target.value)}
             placeholder="Entrez une URL"
         />
-        <div className="qrcode">
+        <div className="qrCodeContainer">
             {value ? (
-                <div className="thecode">
-                    <QRCode 
-                        id="thisQRCode"
-                        title="Liline's Lab"
-                        value={value}
-                        size={165}
-                        level='L'
-                    />
+                <div>
+                    <div className="qrCode" ref={qrCodeRef}>
+                        <span className='qrName'>{value}</span>
+                        <QRCode 
+                            title="Liline's Lab"
+                            value={value}
+                            size={165}
+                            level='L'
+                        />
+                    </div>
+                    <button onClick={downloadQRCode}>Télécharger le QRCode</button>
                 </div>
             ) : (
                 <p>Le QRCode apparaîtra ici !</p>
